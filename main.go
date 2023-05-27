3e99161b9c42817552665e84f671fa3cf3d7fb0b
falsystack <falsystack@gmail.com>
2023-05-27 17:46:18 +0900

    feat: first commit

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+var baseURL = "https://www.wantedly.com/projects?type=mixed&occupation_types%5B%5D=jp__engineering&keywords%5B%5D=golang&page=1"
+
+func main() {
+	pages := getPages()
+	for i := 1; i <= pages; i++ {
+		getPage(i)
+	}
+}
+
+func getPage(page int) {
+	pageURL := baseURL + "&page=" + strconv.Itoa(page)
+	fmt.Println("リクエストURL：", pageURL)
+}
+
+func getPages() int {
+	pages := 0
+	resp, err := http.Get(baseURL)
+	hasErr(err)
+	hasErrCodes(resp)
+
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	hasErr(err)
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		// paginationクラス中の全てのaタグを探し出す。
+		pages = s.Find("a").Length()
+	})
+	return pages
+}
+
+func hasErrCodes(resp *http.Response) {
+	if resp.StatusCode != 200 {
+		log.Fatalln("request failed with status : ", resp.StatusCode)
+	}
+}
+
+func hasErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
