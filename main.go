495fe7f5fd1e827be5f56e63b281f2d337b254df
falsystack <falsystack@gmail.com>
2023-05-29 18:00:16 +0900

    feat: serving file with http.ServeFile

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "scraper/scrapper"
+import (
+	"log"
+	"net/http"
+	"os"
+	"scraper/scrapper"
+	"scraper/utils"
+	"strings"
+)
 
 func main() {
-	scrapper.Scrape("golang")
+	http.Handle("/", http.FileServer(http.Dir("./resources")))
+	http.HandleFunc("/scrape", scrapeJobs)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func scrapeJobs(w http.ResponseWriter, req *http.Request) {
+	defer os.Remove("jobs.csv")
+	err := req.ParseForm()
+	utils.HasErr(err)
+
+	term := req.FormValue("term")
+	log.Println(term)
+	term = strings.ToLower(utils.CleanString(term))
+	scrapper.Scrape(term)
+
+	http.ServeFile(w, req, "jobs.csv")
 }
